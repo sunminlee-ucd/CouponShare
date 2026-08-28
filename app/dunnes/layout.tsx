56c23be55348635bd0cbb5b9f8077c2c8bddc215
sunminlee-ucd <sunmin.lee@ucdconnect.ie>
2026-08-28 14:00:03 +0100

    Mount Dunnes usage guide

--- app/dunnes/layout.tsx
+++ app/dunnes/layout.tsx
@@ -1,7 +1,8 @@
 import type { ReactNode } from "react";
 import DunnesBarcodeEnhancer from "./DunnesBarcodeEnhancer";
 import DunnesGuestActionGuard from "./DunnesGuestActionGuard";
+import DunnesUsageGuide from "./DunnesUsageGuide";
 
 export default function DunnesLayout({ children }: { children: ReactNode }) {
-  return <><DunnesBarcodeEnhancer /><DunnesGuestActionGuard />{children}</>;
+  return <><DunnesBarcodeEnhancer /><DunnesGuestActionGuard /><DunnesUsageGuide />{children}</>;
 }
